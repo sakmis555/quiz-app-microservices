9eb5460382973f935b07752f75be2b93c5b7bfbf
Saksham Mishra <sakmis555@gmail.com>
2024-07-18 16:50:28 +0530

    basic question routes added

---
+++ question-service/src/main/java/com/saksham/questionservice/controller/QuestionController.java
@@ -0,0 +1,47 @@
+package com.saksham.questionservice.controller;
+
+import com.saksham.questionservice.model.Question;
+import com.saksham.questionservice.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return questionService.getAllQuestions();
+    };
+
+    @GetMapping("{questionId}")
+    public ResponseEntity<Question> getQuestion(@PathVariable int questionId) {
+        return questionService.getQuestionById(questionId);
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    };
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<Question> updateQuestion(@RequestBody Question question) {
+        return questionService.updateQuestion(question);
+    };
+
+    @DeleteMapping("delete/{questionId}")
+    public ResponseEntity<Question> deleteQuestion(@PathVariable int questionId) {
+        return questionService.deleteQuestion(questionId);
+    }
+}
---
+++ question-service/src/main/java/com/saksham/questionservice/service/QuestionService.java
@@ -0,0 +1,76 @@
+package com.saksham.questionservice.service;
+
+import com.saksham.questionservice.dao.QuestionDao;
+import com.saksham.questionservice.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    };
+
+    public ResponseEntity<Question> getQuestionById(int questionId) {
+        try {
+            return new ResponseEntity<>(questionDao.findById(questionId).orElse(new Question()), HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new Question(), HttpStatus.BAD_REQUEST);
+    };
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findAllByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionDao.save(question);
+            return new ResponseEntity<>("Question Added Successfully", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Question Addition Failed", HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<Question> updateQuestion(Question question) {
+        try {
+            questionDao.save(question);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return getQuestionById(question.getId());
+    }
+
+    public ResponseEntity<Question> deleteQuestion(int questionId) {
+        ResponseEntity<Question> question = null;
+        try {
+            question = getQuestionById(questionId);
+            questionDao.deleteById(questionId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return question;
+    }
+}
